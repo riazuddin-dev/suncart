e85aa13aa53d1391bd7d68fa0c742df48441a788
riazut <riazuddin7209@gmail.com>
2026-05-03 15:49:06 +0600

    fix error

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -23,10 +23,6 @@ socialProviders: {
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   },
-  github: {
-    clientId: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  },
 },
  
 });
